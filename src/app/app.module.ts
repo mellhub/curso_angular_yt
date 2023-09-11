9ddf1e603fb8335d437fef9a20a2f80c7cf8d2f7
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-09-11 15:56:19 -0400

    curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,44 @@
+import { EditMomentComponent } from './components/pages/edit-moment/edit-moment.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { MomentComponent } from './components/pages/moment/moment.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MessagesComponent } from './components/messages/messages.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    MomentComponent,
+    AboutComponent,
+    NewMomentComponent,
+    MessagesComponent,
+    EditMomentComponent,
+    MomentFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+  
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
